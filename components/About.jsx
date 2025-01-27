e012e824435316a48c319960bfc536d948a2dc4a
unknown <SCHW18TZ>
2025-01-27 21:42:19 +0530

    changed icons

--- components/About.jsx
+++ components/About.jsx
@@ -28,33 +28,17 @@ function About() {
         className="mt-4 dark:text-ctnSecondaryDark text-gray-300 text-[17px] w-full leading-[30px] flex flex-col justify-between gap-6"
       >
         <div>
-          Hello! I&apos;m Harsh,I am an independent software developer based in India with expertise in building scalable and efficient solutions. My technical skill set includes TypeScript, JavaScript, React, Python, C++, C#, and Java. With a passion for clean code and innovative problem-solving, I enjoy creating web and software applications that combine functionality with great user experiences.
+          Hello! I&apos;m Harsh,I am an independent software developer based in
+          India with expertise in building scalable and efficient solutions. My
+          technical skill set includes TypeScript, JavaScript, React, Python,
+          C++, C#, and Java. With a passion for clean code and innovative
+          problem-solving, I enjoy creating web and software applications that
+          combine functionality with great user experiences.
           <br className="sm:block hidden" />
           Let&apos;s collaborate to bring your ideas to life!
         </div>
-        <div className="w-fit break-words">
-          <Link
-            href="mailto:harshh.tsx@gmail.com"
-            target="_blank"
-            rel="noreferrer"
-            className="hover:text-primary w-full transition-all duration-100 ease-in flex md:items-center gap-2 md:flex-row flex-wrap word-break hover:-translate-y-2"
-          >
-            <EmailIcon className="w-[30px] h-[30px]" />
-            harshh.tsx@gmail.com
-          </Link>
-        </div>
-        <div className="flex gap-5 items-center">
-          {socials.map((social) => (
-            <Link
-              href={social.link}
-              target="_blank"
-              key={social.id}
-              className="w-8 h-8 hover:-translate-y-2 ease-in transition-all duration-100 cursor-pointer"
-            >
-              {social.icon}
-            </Link>
-          ))}
-        </div>
+        <div className="w-fit break-words"></div>
+        <div className="flex gap-5 items-center"></div>
       </motion.div>
     </section>
   );
